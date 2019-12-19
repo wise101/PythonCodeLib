956bb0aaaf85b111bb12ce761ea628358ded555b
zhujincan <zhujincan@piesat.cn>
2019-12-19 15:39:22 +0800

    *) 遍历文件夹时忽略隐藏文件和隐藏文件夹

--- OrthoHttpTest/util/filelib.py
+++ OrthoHttpTest/util/filelib.py
@@ -3,7 +3,7 @@
 
 import os
 
-def get_file_path(root_path,file_list,dir_list):
+def traverse_folder(root_path,file_list,dir_list):
     #获取该目录下所有的文件名称和目录名称
     # dir_or_files = os.listdir(root_path)
     # for dir_file in dir_or_files:
@@ -21,11 +21,10 @@ def get_file_path(root_path,file_list,dir_list):
         dirs[:] = [d for d in dirs if not d[0] == '.']
         # use files and dirs
         for file_name in files:
-            dir_file_path = os.path.join(root_path, file_name)
-                #判断该路径为文件还是路径
-            if os.path.isdir(dir_file_path):
-                dir_list.append(dir_file_path)
-                #递归获取所有文件和目录的路径
-                get_file_path(dir_file_path,file_list,dir_list)
-            else:
-                file_list.append(dir_file_path)
+            dir_file_path = os.path.join(root, file_name)
+            file_list.append(dir_file_path)
+
+        for dir_name in dirs:
+            dir_path = os.path.join(root, dir_name)
+            dir_list.append(dir_path)
+            traverse_folder(dir_path,file_list,dir_list)
