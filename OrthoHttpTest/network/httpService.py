810e080f556ae45229e187d07e32348ff4679167
zhujincan <zhujincan@piesat.cn>
2019-12-24 09:50:09 +0800

    *) 添加GetTest函数

--- OrthoHttpTest/network/httpService.py
+++ OrthoHttpTest/network/httpService.py
@@ -6,6 +6,7 @@ import requests
 #参考资料
 #Python - 优雅而简单地完成http请求
 #http://baijiahao.baidu.com/s?id=1599992188940440730&wfr=spider&for=pc
+#https://www.cnblogs.com/jesseZh/p/4701266.html
 #  测试参数
 #  url = "http://tcc.taobao.com/cc/json/mobile_tel_segment.htm?"
 #  json_data =  {"tel":"15850781443"}
@@ -13,4 +14,9 @@ def PostTest(url,json_data):
     # 发送post请求
     #json_data = {"user": "yunweicai", "op": "post"}
     r11 = requests.post(url,json_data)
+    print(r11.text)
+
+def GetTest(url):
+    #发送get请求
+    r11 = requests.get(url)
     print(r11.text)
