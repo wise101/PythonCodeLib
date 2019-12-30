6978fb0122e575e80a61d1c4cd5535c1cba8b982
zhujincan <zhujincan@piesat.cn>
2019-12-30 17:30:00 +0800

    *) 添加调试信息

--- OrthoHttpTest/geo_tool/searchRefImg.py
+++ OrthoHttpTest/geo_tool/searchRefImg.py
@@ -31,6 +31,7 @@ def PixelToWorld_Ex(adfGeoTransform,lCol, lRow, pt):
     pt.ptY = adfGeoTransform[3] + lCol * adfGeoTransform[4] + lRow * adfGeoTransform[5]
 
 def GetRasterEnv(imgFile,env):
+    print(imgFile)
     dataset = gdal.Open(imgFile,1)
 #   print(imgFile, ' 宽=', dataset.RasterXSize)
     adfGeoTransform = dataset.GetGeoTransform()
